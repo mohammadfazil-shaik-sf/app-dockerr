a517d3b7ae85e1938b8edd9c3f181a65dee93ea5
mohammadfazil-shaik-sf <mohammadfazil.shaik@heroku.com>
2020-02-13 11:49:34 +0530

    com

--- index.js
+++ index.js
@@ -40,11 +40,26 @@ app.get('/times', (req, res) => res.send(showTimes()))
 
   http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// showTimes = () => {
-//   let result = ''
-//   const times = process.env.TIMES || 5
-//   for (i = 0; i < times; i++) {
-//     result += i + ' '
-//   }
-//   return result;
-// }
+showTimes = () => {
+   let result = ''
+   const times = process.env.TIMES || 5
+   for (i = 0; i < times; i++) {
+     result += i + ' '
+   }
+   return result;
+}
+var url   = require('url');
+var Redis = require('ioredis');
+
+redis_uri = url.parse(process.env.REDIS_URL);
+var redis = new Redis({
+  port: Number(redis_uri.port) + 1,
+  host: redis_uri.hostname,
+  password: redis_uri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+});
