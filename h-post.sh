686970806326da9cb7b3bbad220b79fd307a71af
mohammadfazil-shaik-sf <mohammadfazil.shaik@heroku.com>
2020-02-14 07:01:52 +0530

    push

--- h-post.sh
+++
@@ -1,2 +0,0 @@
-echo $HEROKU_APP_NAME
-export CLIENT_HOST=https://$HEROKU_APP_NAME.herokuapp.com
--- showtime.js
+++
@@ -1,8 +0,0 @@
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
